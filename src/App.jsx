dc2086e2733948cc2c2b4984cf1b179026d56642
0x0zgdo <zgdotech@gmail.com>
2025-04-28 11:21:25 -0400

    marasa ako heading

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,10 @@ function App() {
   return (
     <>
      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          Mas marasa ako
-        </p>
-      </div>
+      <h1>Mas marasa ako kesa kan Gab!</h1>
+    
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Mas marasa ako!!
       </p>
     </>
   )
